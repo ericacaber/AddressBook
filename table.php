c82a9f378d08ecb90fab2ca2cd03642d947587ef
Erica Caber <ericacaber@klab.com>
2013-03-12 16:39:18 +0800

    Revert "Revert "added comment""
    This reverts commit d7aeb84b76ece0e0c75d545795babda687f58db8.

--- table.php
+++ table.php
@@ -16,6 +16,10 @@ $num = mysql_numrows($result);
 mysql_close();
 
 
+
+//added comment
+//added comment
+
 echo "<b><center>Database Output</center></b></br>";
 echo '<table border="1" cellspacing="2" cellpadding="2">';
 echo '<tr>
